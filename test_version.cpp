db9d036a6ce9f4231e734a1f5587db34de34fd4f
reficul0 <cheshirwr@mail.ru>
2020-10-31 15:21:22 +0700

    no message

--- test_version.cpp
+++ test_version.cpp
@@ -1,7 +1,5 @@
 #define BOOST_TEST_MODULE helloworld_test_module
 
-#include "lib.h"
-
 #include <iostream>
 #include <stdlib.h>
 #include <boost/test/unit_test.hpp>
@@ -10,14 +8,9 @@
 BOOST_AUTO_TEST_SUITE(helloworld_test_suite)
 
 BOOST_AUTO_TEST_CASE(helloworld_test_version)
-{
-	BOOST_TEST_MESSAGE("Version: " << boost::lexical_cast<std::string>(version()) << ";");
-}
+{}
 
 BOOST_AUTO_TEST_CASE(helloworld_test_build)
-{
-	auto *travis_build_number_as_str = getenv("TRAVIS_BUILD_NUMBER");
-	BOOST_TEST_MESSAGE("Build number: " << travis_build_number_as_str << ";");
-}
+{}
 
 BOOST_AUTO_TEST_SUITE_END()
